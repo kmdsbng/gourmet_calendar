086356916e6868099fcd23dd65b7e52146c88e41
Yoshihiro Kameda <kameda.sbng@gmail.com>
2014-09-17 21:04:06 +0900

    leafのコンテンツパーサ追加

--- scripts/event_source_crawler/load_event_source.rb
+++ scripts/event_source_crawler/load_event_source.rb
@@ -7,13 +7,38 @@ require 'nokogiri'
 FileUtils.cd(File.dirname(__FILE__))
 require './../../config/environment'
 
-module EventSourceImporter
-end
+class EventSourceImporter
+  def import(url)
+    source_type = detect_source_type(url)
+    parser = get_parser(source_type)
+    content = load_web_content(url)
+    event_source_attr = parser.parse(content)
+    save_event_source(url, source_type, event_source_attr)
+  end
 
-class EventSourceImporter::LeafEventSourceImporter
+  def save_event_source(url, source_type, event_source_attr)
+  end
+
+  def detect_source_type
+    raise 'TODO: implement'
+  end
 end
 
 class EventSourceImporter::LeafEventSourceParser
+  def parse(content)
+    doc = Nokogiri::HTML(content)
+    title = doc.title.maybe.split('イベント詳細')[0].end.to_s.strip
+    dts = doc.xpath('//dt')
+    basyo_dt = dts.detect {|e| e.text.strip == '場所'}
+    place_str = basyo_dt.maybe.next_element.text.strip.end
+    kikan_dt = dts.detect {|e| e.text.strip == '期間'}
+    range_str = kikan_dt.maybe.next_element.text.strip.gsub(/[\t\r\n]/, '').end
+    {
+      title: title,
+      place_str: place_str,
+      range_str: range_str,
+    }
+  end
 end
 
 def main
@@ -54,6 +79,38 @@ case $PROGRAM_NAME
 when __FILE__
   main
 when /spec[^\/]*$/
-  # {spec of the implementation}
+  describe EventSourceImporter::LeafEventSourceParser do
+    before do
+      url = 'http://www.leafkyoto.net/event/detail/496'
+      content = load_web_content(url)
+      parser = EventSourceImporter::LeafEventSourceParser.new
+      @attr = parser.parse(content)
+    end
+
+    it "has valid title" do
+      expect(@attr[:title]).to eq("極上肉を堪能！「第1回 京都肉祭」開催決定！")
+    end
+
+    it "has valid place" do
+      expect(@attr[:place_str]).to eq("［京都市役所前広場］（京都市・寺町御池）")
+    end
+
+    it "has valid range" do
+      expect(@attr[:range_str]).to eq("2014年09月27日(土)~2014年09月27日(土)")
+    end
+  end
+
+  describe EventSourceImporter do
+    before do
+      @importer = EventSourceImporter.new
+    end
+
+    it "detect leafkyoto.net url" do
+      url = 'http://www.leafkyoto.net/event/detail/496'
+      expect(@importer.detect_source_type(url)).to eq('leaf')
+    end
+
+  end
+
 end
 
