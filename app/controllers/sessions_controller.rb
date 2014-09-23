7740d59c151ff876d7a62225cf7f9985cd435879
亀田 義裕 <kameda.sbng@gmail.com>
2014-09-23 15:00:49 +0900

    twitter認証処理実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def create
+    auth = request.env['omniauth.auth']
+
+    provider = auth[:provider]
+    nickname = auth[:info][:nickname]
+
+    unless acceptable_user?(provider, nickname)
+      redirect_to root_path, notice: '許可されたユーザでないのでログインできません'
+    end
+
+    user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to root_path, notice: 'ログインしました'
+  end
+
+  private
+  def acceptable_user?(provider, nickname)
+    n
+    acceptables = Set.new([['twitter', 'kmdsbng'], ['twitter', 'minotami']])
+    acceptables.include?([provider, nickname])
+  end
+end
