791df7f7df3696574fda078872fbce6a0672ac37
亀田 義裕 <kameda.sbng@gmail.com>
2014-09-26 20:37:29 +0900

    implement event source load.

--- app/controllers/event_sources_controller.rb
+++ app/controllers/event_sources_controller.rb
@@ -1,12 +1,23 @@
 class EventSourcesController < ApplicationController
 
   def todos_json
-    render :json => {
-      eventSources: [
-        {title: 'aaa'   , url: 'http://www.yahoo.co.jp', place: 'kyoto', range: 'iPhone 5'},
-        {title: 'Sporting Goods', url: 'http://www.yahoo.co.jp' , place: 'kyoto', range: 'Basketball'},
-        {title: 'Electronics'   , url: 'http://www.yahoo.co.jp', place: 'kyoto', range: 'iPhone 5'},
-      ]
+    models = ::EventSource.all
+    model_jsons = to_jsons(models)
+    render :json => { eventSources: model_jsons }
+  end
+
+  private
+  def to_jsons(models)
+    models.map {|model|
+      {
+        id: model.id,
+        title: model.title,
+        url: model.url,
+        place: model.place_str,
+        range: model.range_str,
+        import_success: model.import_success,
+        ignored: model.ignored,
+      }
     }
   end
 end
